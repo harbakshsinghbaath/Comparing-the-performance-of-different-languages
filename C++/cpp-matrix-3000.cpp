a0c49632952043f7d59a52027350c1069af0b002
Harbaksh Singh Baath <harbakshsinghbaath@gmail.com>
2025-01-09 17:52:15 +0530

    cpp done took max 7 mins not bad

---
+++ C++/cpp-matrix-3000.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+using namespace std;
+int main() {
+    const int size = 3000;
+    vector<vector<int>> arr1(size, vector<int>(size));
+    vector<vector<int>> arr2(size, vector<int>(size));
+    vector<vector<int>> arrResult(size, vector<int>(size));
+
+    // Initialize arrays
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            arr1[i][j] = i + j;
+            arr2[i][j] = i - j;
+        }
+    }
+
+    auto start = chrono::high_resolution_clock::now();
+
+    // Matrix multiplication
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            arrResult[i][j] = 0;
+            for (int k = 0; k < size; ++k) {
+                arrResult[i][j] += arr1[i][k] * arr2[k][j];
+            }
+        }
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start;
+
+    cout << "Execution time: " << duration.count() << " seconds" << endl;
+
+    return 0;
+}
+//execution time is 436.531 seconds
