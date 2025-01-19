a5d80b8eaf6ec40bf4ba1974c3db4d7b57150434
Harbaksh Singh Baath <harbakshsinghbaath@gmail.com>
2025-01-19 23:58:56 +0530

    matlab

---
+++ MATLAB/m_matrix_2000.m
@@ -0,0 +1,21 @@
+
+size = 2000;
+
+% Initialize the matrices with some values
+matrix1 = zeros(size, size);
+matrix2 = zeros(size, size);
+
+for i = 1:size
+    for j = 1:size
+        matrix1(i, j) = i + j;
+        matrix2(i, j) = i - j;
+    end
+end
+
+% Perform matrix multiplication
+tic; % Start timer
+result = matrix1 * matrix2;
+elapsedTime = toc; % End timer and get elapsed time
+
+% Display the elapsed time
+fprintf('Elapsed time for matrix multiplication: %.2f seconds\n', elapsedTime);
